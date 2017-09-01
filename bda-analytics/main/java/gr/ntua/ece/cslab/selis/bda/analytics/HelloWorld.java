3e913356b41888cf467d5d9b6b0f96fe9e0e315c
nchalv <nchalv@cslab.ece.ntua.gr>
2017-09-01 16:09:29 +0300

    Eclipse integration

---
+++ bda-analytics/main/java/gr/ntua/ece/cslab/selis/bda/analytics/HelloWorld.java
@@ -0,0 +1,7 @@
+package java.gr.ntua.ece.cslab.selis.bda.analytics;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello SELIS World!");
+    }
+}
