f79e479065ccadd01706c38fee40cb1aa8b45326
nchalv <nchalv@cslab.ece.ntua.gr>
2017-09-01 16:09:29 +0300

    Eclipse integration

---
+++ bda-analytics/test/java/gr/ntua/ece/cslab/selis/bda/analytics/HelloWorldTest.java
@@ -0,0 +1,19 @@
+package java.gr.ntua.ece.cslab.selis.bda.analytics;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Giannis Giannakopoulos on 8/31/17.
+ */
+public class HelloWorldTest {
+    @org.junit.Before
+    public void setUp() throws Exception {
+    }
+
+    @org.junit.After
+    public void tearDown() throws Exception {
+    }
+
+
+
+}
