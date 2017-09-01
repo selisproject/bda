34fdf50a6627a1f52c4ab0a177c7009bf9d73225
nchalv <nchalv@cslab.ece.ntua.gr>
2017-09-01 19:09:14 +0300

    Hello

--- bda-analytics/src/main/java/gr/ntua/ece/cslab/selis/bda/analytics/HelloWorld.java
+++ bda-analytics/src/main/java/gr/ntua/ece/cslab/selis/bda/analytics/HelloWorld.java
@@ -2,6 +2,6 @@ package gr.ntua.ece.cslab.selis.bda.analytics;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello SELIS World!");
+        System.out.println("Hello to the SELIS World!");
     }
 }
