99cdd913409f94af6a3c72fb81871387b4195211
Giannis Giannakopoulos <ggian@cslab.ece.ntua.gr>
2017-09-05 15:50:53 +0300

    parsing properties and added script based execution

--- bda-controller/src/main/java/gr/ntua/ece/cslab/selis/bda/controller/Entrypoint.java
+++ bda-controller/src/main/java/gr/ntua/ece/cslab/selis/bda/controller/Entrypoint.java
@@ -2,17 +2,27 @@ package gr.ntua.ece.cslab.selis.bda.controller;
 
 import org.eclipse.jetty.server.Server;
 
+import java.io.FileReader;
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.util.Properties;
+import java.util.logging.Logger;
+
 /**
  * Created by Giannis Giannakopoulos on 8/31/17.
  */
 public class Entrypoint {
-    public static void main(String[] args) {
-        System.out.println("Hello SELIS World!");
-        Server server = new Server(8080);
-
-        // handlers
-        // server.handle();
-
+    private final static Logger LOGGER = Logger.getLogger(Entrypoint.class.getSimpleName());
+    public static void main(String[] args) throws IOException {
+        if(args.length < 1){
+            System.err.println("Please provide a configuration file as a first argument");
+            System.exit(1);
+        }
+        Properties conf = new Properties();
+        conf.load(new FileReader(args[0]));
+        Server server = new Server(
+                new InetSocketAddress(conf.getProperty("server.address"),
+                new Integer(conf.getProperty("server.port"))));
         try {
             server.start();
             server.join();
---
+++ bda-controller/src/main/scripts/selis-bda-server.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+ROOT_DIR="target/"
+LIB_DIR="${ROOT_DIR}lib"
+#JAR_FILE="$(ls ${ROOT_DIR}/bda-controller-*.jar)"
+JAR_FILE="$(echo ${ROOT_DIR}/classes)" # used for debugging only
+ENTRYPOINT="gr.ntua.ece.cslab.selis.bda.controller.Entrypoint"
+CONF_FILE="${ROOT_DIR}/conf/bda.properties"
+
+CLASSPATH="$(ls $LIB_DIR/* | tr '\n' ':')$JAR_FILE"
+java -cp $CLASSPATH $ENTRYPOINT $CONF_FILE
