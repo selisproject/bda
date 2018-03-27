43e85bd700e86523d462bf17dea1ab4bd13452e1
nchalv <nchalv@cslab.ece.ntua.gr>
2018-03-27 21:19:38 +0300

    Spark + KPI DB integration

--- bda-analytics/src/main/java/gr/ntua/ece/cslab/selis/bda/analytics/kpis/Kpi.java
+++ bda-analytics/src/main/java/gr/ntua/ece/cslab/selis/bda/analytics/kpis/Kpi.java
@@ -6,7 +6,10 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.lang.ProcessBuilder.Redirect;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 
 import gr.ntua.ece.cslab.selis.bda.analytics.basicObjects.KpiDescriptor;
 import gr.ntua.ece.cslab.selis.bda.datastore.KPIBackend;
@@ -32,67 +35,89 @@ public class Kpi implements Runnable {
 		return kpiInfo;
 	}
 
-	
 	public void run() {
-        try {
-/*                      String result = "";
-                // System.out.println("Inside");
-                Runtime r = Runtime.getRuntime();
-                String command = kpiInfo.getExecutable().getExecutEngine().getExecutionPreamble() + " "
-                                + kpiInfo.getExecutable().getOsPath();
-                Process p;
-                // System.out.println("Inside");
+		try {
+			/*
+			 * String result = ""; // System.out.println("Inside"); Runtime r =
+			 * Runtime.getRuntime(); String command =
+			 * kpiInfo.getExecutable().getExecutEngine().getExecutionPreamble() + " " +
+			 * kpiInfo.getExecutable().getOsPath(); Process p; //
+			 * System.out.println("Inside");
+			 * 
+			 * p = r.exec(command); //p = r.exec("echo \"hi\""); p.waitFor(); //
+			 * System.out.println("Dead");
+			 * 
+			 * BufferedReader in = new BufferedReader(new
+			 * InputStreamReader(p.getInputStream())); String inputLine; while ((inputLine =
+			 * in.readLine()) != null) { System.out.println(inputLine); result += inputLine;
+			 * } in.close();
+			 */
+			List<String> cmd = new ArrayList<String>(getKpiInfo().getArguments());
+			cmd.add(0, kpiInfo.getExecutable().getExecutEngine().getExecutionPreamble());
+			cmd.add(kpiInfo.getExecutable().getOsPath());
+			ProcessBuilder pb = new ProcessBuilder(cmd);
+			// pb.directory(new File("/home/hduser/nchalv/"));
+			// System.out.println(pb.directory());
+			// File err = new File("err");
+			// File out = new File("out");
+			// pb.redirectError(Redirect.appendTo(err));
+			// pb.redirectOutput(Redirect.appendTo(out));
+			Process p = pb.start();
+			// assert pb.redirectInput() == Redirect.PIPE;
+			// assert pb.redirectOutput().file() == log;
+			// assert p.getInputStream().read() == -1;
+			BufferedReader reader = new BufferedReader(new InputStreamReader(p.getInputStream()));
+			StringBuilder builder = new StringBuilder();
+			String line = null;
+			while ((line = reader.readLine()) != null) {
+				if (line.startsWith("FINAL RESULT: ")) {
+					builder.append(line);
+					builder.append(System.getProperty("line.separator"));
+				}
+			}
+			String result = builder.toString();
+			p.waitFor();
+			// System.out.println(result);
+			result = result.replaceAll("FINAL RESULT: ", "");
+			// System.out.println(result);
+			JSONParser parser = new JSONParser();
+			JSONObject jsonObj = (JSONObject) parser.parse(result);
+			HashMap<String, String> hmap = new HashMap<String, String>();
+			for (Object key : jsonObj.keySet()) {
+				// based on you key types
+				String keyStr = (String) key;
+				Object keyvalue = jsonObj.get(keyStr);
+				hmap.put(keyStr, keyvalue.toString());
+				// Print key and value
+				// System.out.println("key: "+ keyStr + " value: " + keyvalue);
 
-                p = r.exec(command);
-                //p = r.exec("echo \"hi\"");
-                p.waitFor();
-                // System.out.println("Dead");
-
-                BufferedReader in = new BufferedReader(new InputStreamReader(p.getInputStream()));
-                String inputLine;
-                while ((inputLine = in.readLine()) != null) {
-                        System.out.println(inputLine);
-                        result += inputLine;
-                }
-                in.close();*/
-                List<String> cmd = new ArrayList<String>(getKpiInfo().getArguments());
-                cmd.add(0, kpiInfo.getExecutable().getExecutEngine().getExecutionPreamble());
-                cmd.add(kpiInfo.getExecutable().getOsPath());
-                ProcessBuilder pb = new ProcessBuilder(cmd);
-                //pb.directory(new File("/home/hduser/nchalv/"));
-                //System.out.println(pb.directory());
-                File err = new File("err");
-                File out = new File("out");
-                pb.redirectError(Redirect.appendTo(err));
-                pb.redirectOutput(Redirect.appendTo(out));
-                Process p = pb.start();
-                //assert pb.redirectInput() == Redirect.PIPE;
-                //assert pb.redirectOutput().file() == log;
-                //assert p.getInputStream().read() == -1;
-                p.waitFor();
-
-        } catch (Exception e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-        }
+				// for nested objects iteration if required
+				// if (keyvalue instanceof JSONObject)
+				// printJsonObject((JSONObject)keyvalue);
+			}
+			this.store(hmap);
+			//hmap.forEach((k, v) -> System.out.println("key: " + k + " value:" + v));
+			// System.out.println(hmap);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
 	}
 
-	public void store(int value) throws Exception {
-		String fs_string = "jdbc:postgresql://147.102.4.108:5432/sonae";
+	public void store(HashMap<String, String> hmap) throws Exception {
+		String fs_string = "jdbc:postgresql://10.0.1.4:5432/sonae";
 		String uname = "clms";
 		String passwd = "sonae@sEl1s";
 
 		KPIBackend kpiDB = new KPIBackend(fs_string, uname, passwd);
-
-		List<KeyValue> data = new LinkedList<>();
-		/*data.add(new KeyValue("fromdate", Long.toString(fromdate)));
-		data.add(new KeyValue("todate", Long.toString(todate)));
-		data.add(new KeyValue("supplierid", Integer.toString(rn.nextInt(10) + 1)));
-		data.add(new KeyValue("warehouseid", Integer.toString(rn.nextInt(10) + 1)));
-		data.add(new KeyValue("output", "output string"));
-		*/
-		KPIDescription newkpi = new KPIDescription("sonaekpi_0", System.currentTimeMillis(), data);
+		List<KeyValue> data = new LinkedList<KeyValue>();
+		hmap.forEach((k, v) -> data.add(new KeyValue(k, v)));
+//		for	(KeyValue element : data) {
+//			System.out.println(element.getKey());
+//			System.out.println(element.getValue());
+//		}
+		KPIDescription newkpi = new KPIDescription("sonae_orderforecast", System.currentTimeMillis(), data);
 
 		kpiDB.insert(newkpi);
 
