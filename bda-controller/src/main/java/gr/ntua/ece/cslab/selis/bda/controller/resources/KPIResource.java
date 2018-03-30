e7ad21b45200a249be629d720231b0ba2469ce2c
Nikos <nprov@cslab.ece.ntua.gr>
2018-03-30 20:05:57 +0300

    Changes in form of REST API

--- bda-controller/src/main/java/gr/ntua/ece/cslab/selis/bda/controller/resources/KPIResource.java
+++ bda-controller/src/main/java/gr/ntua/ece/cslab/selis/bda/controller/resources/KPIResource.java
@@ -1,14 +1,20 @@
 package gr.ntua.ece.cslab.selis.bda.controller.resources;
 
 import com.google.common.base.Splitter;
+import com.google.gson.JsonObject;
+import gr.ntua.ece.cslab.selis.bda.analytics.kpis.OrderForecast;
 import gr.ntua.ece.cslab.selis.bda.analytics.kpis.SonaeKPI;
 import gr.ntua.ece.cslab.selis.bda.controller.Entrypoint;
 import gr.ntua.ece.cslab.selis.bda.datastore.beans.KPIDescription;
 import gr.ntua.ece.cslab.selis.bda.datastore.beans.KeyValue;
 import gr.ntua.ece.cslab.selis.bda.datastore.beans.RequestResponse;
 import gr.ntua.ece.cslab.selis.bda.datastore.beans.Tuple;
+import org.apache.avro.Schema;
+import org.apache.htrace.fasterxml.jackson.core.type.TypeReference;
+import org.apache.htrace.fasterxml.jackson.databind.ObjectMapper;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 import org.mortbay.util.ajax.JSON;
 
 import javax.ws.rs.*;
@@ -60,19 +66,21 @@ public class KPIResource {
                         row.setTimestamp(cell.getValue());
                     }
                     if (cell.getKey().contentEquals("supplier_id")) {
-                        row.setSupplier_id(cell.getValue());
+                        row.setSupplier_id(Integer.parseInt(cell.getValue()));
                     }
                     if (cell.getKey().contentEquals("warehouse_id")) {
-                        row.setWarehouse_id(cell.getValue());
+                        row.setWarehouse_id(Integer.parseInt(cell.getValue()));
                     }
                     if (cell.getKey().contentEquals("salesforecast_id")) {
-                        row.setSalesforecast_id(cell.getValue());
+                        row.setSalesforecast_id(Integer.parseInt(cell.getValue()));
                     }
                     if (cell.getKey().contentEquals("result")) {
-                        row.setResult(cell.getValue());
+                        ObjectMapper mapper = new ObjectMapper();
+                        List<OrderForecast> forecast = mapper.readValue(cell.getValue(), new TypeReference<List<OrderForecast>>(){});
+                        row.setResult(forecast);
                     }
                 }
-                System.out.println(row.toString());
+                //System.out.println(row.toString());
                 result.add(row);
             }
             //System.out.println(result);
@@ -96,7 +104,7 @@ public class KPIResource {
             System.out.println("Select parameters given");
             Map<String, String> myfilters = Splitter.on(';').withKeyValueSeparator(":").split(filters);
             for (Map.Entry entry : myfilters.entrySet()) {
-                System.out.println(entry.getKey() + " , " + entry.getValue());
+                //System.out.println(entry.getKey() + " , " + entry.getValue());
                 args.add(new KeyValue(entry.getKey().toString(), entry.getValue().toString()));
             }
         }
@@ -110,16 +118,18 @@ public class KPIResource {
                         row.setTimestamp(cell.getValue());
                     }
                     if (cell.getKey().contentEquals("supplier_id")) {
-                        row.setSupplier_id(cell.getValue());
+                        row.setSupplier_id(Integer.parseInt(cell.getValue()));
                     }
                     if (cell.getKey().contentEquals("warehouse_id")) {
-                        row.setWarehouse_id(cell.getValue());
+                        row.setWarehouse_id(Integer.parseInt(cell.getValue()));
                     }
                     if (cell.getKey().contentEquals("salesforecast_id")) {
-                        row.setSalesforecast_id(cell.getValue());
+                        row.setSalesforecast_id(Integer.parseInt(cell.getValue()));
                     }
                     if (cell.getKey().contentEquals("result")) {
-                        row.setResult(cell.getValue());
+                        ObjectMapper mapper = new ObjectMapper();
+                        List<OrderForecast> forecast = mapper.readValue(cell.getValue(), new TypeReference<List<OrderForecast>>(){});
+                        row.setResult(forecast);
                     }
                 }
                 //System.out.println(row.toString());
