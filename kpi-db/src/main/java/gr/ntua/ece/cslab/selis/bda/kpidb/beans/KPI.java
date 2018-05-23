87feeeafd4a809509eaa109073651341ad48c10b
Nikos Provatas <nprov@cslab.ece.ntua.gr>
2018-05-23 18:43:59 +0300

    Added fields and methods on KPI and KPISchema classes

--- kpi-db/src/main/java/gr/ntua/ece/cslab/selis/bda/kpidb/beans/KPI.java
+++ kpi-db/src/main/java/gr/ntua/ece/cslab/selis/bda/kpidb/beans/KPI.java
@@ -1,4 +1,47 @@
 package gr.ntua.ece.cslab.selis.bda.kpidb.beans;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class KPI {
+    String kpi_name;
+    long timestamp;
+    List<KeyValue> entries;
+
+    /*
+        Empty Constructor
+     */
+    public KPI() {
+        this.entries = new LinkedList<>();
+    }
+
+    public KPI(String kpi_name, long timestamp, List<KeyValue> entries) {
+        this.kpi_name = kpi_name;
+        this.timestamp = timestamp;
+        this.entries = entries;
+    }
+
+    public String getKpi_name() {
+        return kpi_name;
+    }
+
+    public void setKpi_name(String kpi_name) {
+        this.kpi_name = kpi_name;
+    }
+
+    public long getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(long timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public List<KeyValue> getEntries() {
+        return entries;
+    }
+
+    public void setEntries(List<KeyValue> entries) {
+        this.entries = entries;
+    }
 }
--- kpi-db/src/main/java/gr/ntua/ece/cslab/selis/bda/kpidb/beans/KPISchema.java
+++ kpi-db/src/main/java/gr/ntua/ece/cslab/selis/bda/kpidb/beans/KPISchema.java
@@ -1,4 +1,40 @@
 package gr.ntua.ece.cslab.selis.bda.kpidb.beans;
 
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+import java.util.List;
+
+@XmlRootElement(name = "DimensionTableSchema")
+@XmlAccessorType(XmlAccessType.PUBLIC_MEMBER)
 public class KPISchema {
+    private List<String> columnNames;
+    private List<KeyValue> columnTypes;
+
+    /* Empty constructor */
+    public KPISchema() {
+    }
+
+    /* Constructor on fields*/
+    public KPISchema(List<String> columnNames, List<KeyValue> columnTypes) {
+        this.columnNames = columnNames;
+        this.columnTypes = columnTypes;
+    }
+
+    /* Getters and Setters */
+    public List<String> getColumnNames() {
+        return columnNames;
+    }
+
+    public void setColumnNames(List<String> columnNames) {
+        this.columnNames = columnNames;
+    }
+
+    public List<KeyValue> getColumnTypes() {
+        return columnTypes;
+    }
+
+    public void setColumnTypes(List<KeyValue> columnTypes) {
+        this.columnTypes = columnTypes;
+    }
 }
