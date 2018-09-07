ab6d87b7a41dbff64926c43db97524113abf92b6
Tasos Bakogiannis <abk@cslab.ece.ntua.gr>
2018-09-07 18:49:57 +0300

    no more controller.beans.Recipe.

---
+++ bda-controller/src/test/java/gr/ntua/ece/cslab/selis/bda/controller/HelloWorldTest.java
@@ -0,0 +1,362 @@
+package gr.ntua.ece.cslab.selis.bda.controller;
+
+import gr.ntua.ece.cslab.selis.bda.common.Configuration;
+import gr.ntua.ece.cslab.selis.bda.analytics.AnalyticsSystem;
+import gr.ntua.ece.cslab.selis.bda.common.storage.SystemConnector;
+import gr.ntua.ece.cslab.selis.bda.common.storage.SystemConnectorException;
+import gr.ntua.ece.cslab.selis.bda.common.storage.connectors.Connector;
+import gr.ntua.ece.cslab.selis.bda.common.storage.connectors.ConnectorFactory;
+import gr.ntua.ece.cslab.selis.bda.common.storage.connectors.PostgresqlConnector;
+import gr.ntua.ece.cslab.selis.bda.controller.beans.JobDescription;
+import gr.ntua.ece.cslab.selis.bda.controller.beans.MessageType;
+import gr.ntua.ece.cslab.selis.bda.common.storage.beans.Recipe;
+import gr.ntua.ece.cslab.selis.bda.common.storage.connectors.BDAdbPooledConnector;
+import gr.ntua.ece.cslab.selis.bda.controller.resources.JobResource;
+import gr.ntua.ece.cslab.selis.bda.controller.resources.MessageResource;
+import gr.ntua.ece.cslab.selis.bda.controller.resources.RecipeResource;
+import gr.ntua.ece.cslab.selis.bda.datastore.connectors.DatastoreConnector;
+import org.json.JSONObject;
+
+import java.io.*;
+import java.sql.*;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
+/**
+ * Created by Giannis Giannakopoulos on 8/31/17.
+ */
+public class HelloWorldTest {
+
+    /*
+    Logger LOGGER = Logger.getLogger(HelloWorldTest.class.getCanonicalName());
+
+    Configuration configuration;
+    Connector dtConnector, kpiConnector;
+    MessageType msgType;
+    Recipe recipe;
+    JobDescription jobDescription;
+
+    MessageResource messageResource;
+    RecipeResource recipeResource;
+    JobResource jobResource;
+
+
+    private static final String MSG_TYPE_STRING =
+            "{" +
+                    "\"name\":\"exampleMsgType\","+
+                    "\"description\":\"exampleMsgType\"," +
+                    "\"active\": true," +
+                    "\"format\":\"{" +
+                        "'message_type': 'exampleMsgType', " +
+                        "'payload': {" +
+                            "'supplier_id': 'integer'," +
+                            "'sales_forecast': 'jsonb'," +
+                            "'warehouse_id': 'integer'" +
+                        "}" +
+                    "}\"" +
+            "}";
+
+    private static final String RECIPE_STRING =
+            "{"+
+                "\"name\" : \"recipe\"," +
+                "\"description\" : \"recipe\"," +
+                "\"executable_path\" : \"\"," +
+                "\"engine_id\" : 1," +
+                "\"args\" : {" +
+                    "\"intarg\" : 1," +
+                    "\"strarg\" : \"string\"" +
+                "}" +
+            "}";
+
+
+    private static final String MESSAGE = "{" +
+            "'message_type': 'recipe', " +
+            "'payload': {" +
+                "'supplier_id': 1," +
+                "'sales_forecast': {'mpla1' : 1, 'mpla2' : 'mpla'}," +
+                "'warehouse_id': 1" +
+                "}" +
+            "}";
+
+    private static final String DELETE_MSG_TYPE_QUERY =
+            "DELETE FROM metadata.message_type " +
+            "WHERE id = ?;";
+
+    private static final String DELETE_RECIPE_QUERY =
+            "DELETE FROM metadata.recipes " +
+            "WHERE id = ?;";
+
+    private static final String DELETE_JOB_QUERY =
+            "DELETE FROM metadata.jobs " +
+            "WHERE id = ?;";
+
+    private static final String DROP_DATABASE_SCHEMA_QUERY =
+            "DROP SCHEMA IF EXISTS %s CASCADE;";
+    /*
+    private void execute_delete(String query, int id) {
+        Connection connection = BDAdbPooledConnector.getInstance().getBdaConnection();
+
+        try {
+            PreparedStatement statement = connection.prepareStatement(query);
+            statement.setInt(1, id);
+
+            statement.executeUpdate();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private MessageType messageTypeFromString(String msg) {
+        JSONObject obj = new JSONObject(msg);
+
+        return new MessageType(obj.getString("name"),
+                obj.getString("description"),
+                obj.getBoolean("active"),
+                obj.getString("format")
+        );
+
+    }
+
+    private Recipe recipeFromString(String recipe) {
+        JSONObject obj = new JSONObject(recipe);
+
+        return new Recipe(obj.getString("name"),
+                obj.getString("description"),
+                obj.getString("executable_path"),
+                obj.getInt("engine_id"),
+                obj.getJSONObject("args").toString());
+    }
+
+    private void fetch_engines() {
+        LOGGER.log(Level.INFO, "Fetch execution engines for analytics module.");
+        Connection conn = BDAdbPooledConnector.getInstance().getBdaConnection();
+
+        Statement statement;
+        ResultSet engines = null;
+
+        try {
+            statement = conn.createStatement();
+
+            engines = statement.executeQuery("SELECT * FROM metadata.execution_engines;");
+
+            if (engines != null) {
+                while (engines.next()) {
+                    Entrypoint.analyticsComponent.getEngineCatalog().addNewExecutEngine(
+                            engines.getInt("id"),
+                            engines.getString("name"),
+                            engines.getString("engine_path"),
+                            engines.getBoolean("local_engine"),
+                            new JSONObject(engines.getString("args"))
+                    );
+                }
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            conn.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        LOGGER.log(Level.INFO, "Fetched engines : " + Entrypoint.analyticsComponent.
+                getEngineCatalog().getAllExecutEngines() + "\n");
+
+    }
+
+    private void initialize_components() {
+        LOGGER.log(Level.INFO, "Initializing BDADB KPIConnector...");
+        BDAdbPooledConnector.init(
+                Entrypoint.configuration.storageBackend.getBdaDatabaseURL(),
+                Entrypoint.configuration.storageBackend.getDimensionTablesURL(),
+                Entrypoint.configuration.storageBackend.getDbUsername(),
+                Entrypoint.configuration.storageBackend.getDbPassword()
+        );
+
+
+        LOGGER.log(Level.INFO, "Initializing Analytics SubModule...");
+        Entrypoint.analyticsComponent = AnalyticsSystem.getInstance(
+                Entrypoint.configuration.kpiBackend.getDbUrl(),
+                Entrypoint.configuration.kpiBackend.getDbUsername(),
+                Entrypoint.configuration.kpiBackend.getDbPassword()
+        );
+        fetch_engines();
+
+        LOGGER.log(Level.INFO, "Creating folders for uploaded recipes and recipe results");
+
+        File theDir = new File("/uploads/");
+        if (!theDir.exists()) {
+            theDir.mkdir();
+        }
+        theDir = new File("/results/");
+        if (!theDir.exists()) {
+            theDir.mkdir();
+        }
+    }
+
+    private void destroy_fs_data(List<String> files) {
+        for (String path : files) {
+            File theFile = new File(path);
+            if (theFile.exists()) {
+                theFile.delete();
+            }
+        }
+        File theDir = new File("/uploads/");
+        if (theDir.exists()) {
+            theDir.delete();
+        }
+        theDir = new File("/results/");
+        if (theDir.exists()) {
+            theDir.delete();
+        }
+    }
+
+    @org.junit.Before
+    public void setUp() throws Exception {
+        SystemConnector.init("/code/conf/bda.properties");
+
+        /*configuration = Configuration.parseConfiguration("/code/conf/bda.properties");
+
+
+        LOGGER.log(Level.INFO, "About to create schemas in test database");
+
+
+        try {
+            KPIPostgresqlConnector.createSchema(configuration.testDb.getDbUrl(),
+                    configuration.testDb.getDbUsername(),
+                    configuration.testDb.getDbPassword(),
+                    configuration.testDb.getDbUsername(),
+                    "metadata");
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw new SystemConnectorException("Could not create Postgresql schema.");
+        }
+
+
+        dtConnector = KPIConnectorFactory.getInstance().generateConnector(
+                configuration.testDb.getDbUrl(),
+                configuration.testDb.getDbUsername(),
+                configuration.testDb.getDbPassword()
+        );
+
+        try {
+            KPIPostgresqlConnector.createSchema(configuration.testDb.getDbUrl(),
+                    configuration.testDb.getDbUsername(),
+                    configuration.testDb.getDbPassword(),
+                    configuration.testDb.getDbUsername(),
+                    "kpi");
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw new SystemConnectorException("Could not create Postgresql schema.");
+        }
+
+
+        kpiConnector = KPIConnectorFactory.getInstance().generateConnector(
+                configuration.testDb.getDbUrl(),
+                configuration.testDb.getDbUsername(),
+                configuration.testDb.getDbPassword()
+        );
+
+        DatastoreConnector localDtConnector = gr.ntua.ece.cslab.selis.bda.datastore.connectors.
+                KPIConnectorFactory.getInstance().generateConnector(dtConnector);
+
+        localDtConnector.createMetaTables();
+
+//        messageResource = new MessageResource();
+  //    recipeResource = new RecipeResource();
+     //   jobResource = new JobResource();
+
+    //    msgType = messageTypeFromString(MSG_TYPE_STRING);
+
+    }
+
+    @org.junit.Test
+    public void test() throws Exception {
+
+        /*LOGGER.log(Level.INFO, "About to insert new messageType...");
+        messageResource.insert(null , msgType);
+        msgType = MessageType.getMessageByName(msgType.getName());
+        LOGGER.log(Level.INFO, "Inserted : \t" + msgType.toString());
+
+        LOGGER.log(Level.INFO, "About to insert new recipe...");
+        recipeResource.insert(null , RECIPE_STRING);
+        recipe = Recipe.getRecipeByName(recipeFromString(RECIPE_STRING).getName());
+        LOGGER.log(Level.INFO, "Inserted : \t" + recipe.toString());
+
+        LOGGER.log(Level.INFO, "About to upload recipe file...");
+        InputStream uploadedFile = new FileInputStream(new File("/code/examples/recipe.py"));
+        recipeResource.upload(recipe.getId(), recipe.getName() + ".py", uploadedFile);
+        LOGGER.log(Level.INFO, "File uploaded");
+
+        jobDescription = new JobDescription("recipe_job", "recipe_job", true,
+            msgType.getId(), recipe.getId(), "");
+
+        LOGGER.log(Level.INFO, "About to insert new job...");
+        jobResource.insert(null, jobDescription);
+        LOGGER.log(Level.INFO, "After job insertion...");
+        jobDescription = JobDescription.getJobByMessageId(msgType.getId());
+        LOGGER.log(Level.INFO, "Inserted : \t" + jobDescription.toString());
+        LOGGER.log(Level.INFO, "Running recipe with message");
+        Entrypoint.analyticsComponent.run(recipe.getId(), MESSAGE);
+        LOGGER.log(Level.INFO, "Recipe result : ");
+        try (BufferedReader br = new BufferedReader(new FileReader("/results/recipe.out"))) {
+            String line = null;
+            while ((line = br.readLine()) != null) {
+                System.out.println("\t" + line);
+            }
+        }
+        catch (Exception e) {
+            System.out.println(e);
+        }
+        List<Tuple> results = Entrypoint.analyticsComponent.getKpidb().fetch(recipe.getName(), "rows", 1);
+        LOGGER.log(Level.INFO, "KPIDB entry:" );
+        int i = 0;
+        for (Tuple t : results) {
+            LOGGER.log(Level.INFO, "\tResult #" + i);
+            for (KeyValue e : t.getTuple()) {
+                LOGGER.log(Level.INFO, "\t\t" + e.toString());
+            }
+            i++;
+        }
+
+
+    }
+
+    @org.junit.After
+    public void tearDown() throws Exception {
+
+        Connection localConnection = null;
+
+        try {
+            localConnection = DriverManager.getConnection(
+                    configuration.testDb.getDbUrl(),
+                    configuration.testDb.getDbUsername(),
+                    configuration.testDb.getDbPassword()
+            );
+        } catch (SQLException e) {
+            System.out.println("Connection Failed! Check output console");
+            e.printStackTrace();
+            throw e;
+        }
+
+        PreparedStatement statement = localConnection.prepareStatement(
+                String.format(DROP_DATABASE_SCHEMA_QUERY, "metadata"));
+
+        statement.executeUpdate();
+
+        statement = localConnection.prepareStatement(
+                String.format(DROP_DATABASE_SCHEMA_QUERY, "kpi"));
+
+        statement.executeUpdate();
+
+        localConnection.close();
+
+    }
+    */
+}
---
+++ common/src/main/java/gr/ntua/ece/cslab/selis/bda/common/storage/beans/Recipe.java
@@ -0,0 +1,304 @@
+package gr.ntua.ece.cslab.selis.bda.common.storage.beans;
+
+import gr.ntua.ece.cslab.selis.bda.common.storage.SystemConnector;
+import gr.ntua.ece.cslab.selis.bda.common.storage.connectors.PostgresqlConnector;
+
+import java.io.Serializable;
+import java.sql.*;
+import java.util.List;
+import java.util.Vector;
+
+public class Recipe implements Serializable {
+    private final static int DEFAULT_VECTOR_SIZE = 10;
+
+    private transient int id;
+    private String name;
+    private String description;
+    private String executablePath;
+    private int engineId;
+    private String args;
+
+    private boolean exists = false;
+
+    private final static String ALL_RECIPES_QUERY = 
+        "SELECT * " +
+        "FROM metadata.recipes";
+
+    private final static String INSERT_RECIPE_QUERY = 
+        "INSERT INTO metadata.recipes (name, description, executable_path, engine_id, args) " +
+        "VALUES (?, ?, ?, ? ,?::json) " +
+        "RETURNING id";
+
+    private final static String UPDATE_RECIPE_QUERY = 
+        "UPDATE metadata.recipes " +
+        "SET name = ?, description = ?, executable_path = ?, engine_id = ?, args = ?::json " +
+        "WHERE id = ?";
+
+    private final static String GET_RECIPE_BY_ID =
+            "SELECT * FROM metadata.recipes WHERE id = ?;";
+
+    private final static String GET_RECIPE_BY_NAME =
+            "SELECT * FROM metadata.recipes WHERE name = ?;";
+
+    private final static String SET_EXECUTABLE_PATH =
+            "UPDATE metadata.recipes SET executable_path = ? WHERE id = ?;";
+
+    public Recipe() {}
+
+    public Recipe(String name, String description, String executablePath, int engineId, String args) {
+        this.name = name;
+        this.description = description;
+        this.executablePath = executablePath;
+        this.engineId = engineId;
+        this.args = args;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getExecutablePath() {
+        return executablePath;
+    }
+
+    public void setExecutablePath(String executablePath) {
+        this.executablePath = executablePath;
+    }
+
+    public int getEngineId() {
+        return engineId;
+    }
+
+    public void setEngineId(int engineId) {
+        this.engineId = engineId;
+    }
+
+    public String getArgs() {
+        return args;
+    }
+
+    public void setArgs(String args) {
+        this.args = args;
+    }
+
+    public boolean isExists() {
+        return exists;
+    }
+
+    public void setExists(boolean exists) {
+        this.exists = exists;
+    }
+
+    @Override
+    public String toString() {
+        return "Recipe{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", executablePath='" + executablePath + '\'' +
+                ", engineId=" + engineId +
+                ", args=" + args +
+                ", exists=" + exists +
+                '}';
+    }
+
+    public static List<Recipe> getRecipes(String slug) throws SQLException {
+        PostgresqlConnector connector = (PostgresqlConnector ) 
+            SystemConnector.getInstance().getDTconnector(slug);
+
+        Connection connection = connector.getConnection();
+
+        Vector<Recipe> recipes = new Vector<Recipe>(DEFAULT_VECTOR_SIZE);
+
+        try {
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(ALL_RECIPES_QUERY);
+
+            while (resultSet.next()) {
+
+                Recipe recipe;
+                recipe = new Recipe(
+                    resultSet.getString("name"),
+                    resultSet.getString("description"),
+                    resultSet.getString("executable_path"),
+                    resultSet.getInt("engine_id"),
+                    resultSet.getString("args")
+                );
+
+                recipe.id = resultSet.getInt("id");
+                recipe.exists = true;
+
+                recipes.addElement(recipe);
+            }
+
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw e;
+        }
+
+        return recipes;
+     }
+
+    public static Recipe getRecipeById(String slug, int id) {
+        PostgresqlConnector connector = (PostgresqlConnector ) 
+            SystemConnector.getInstance().getDTconnector(slug);
+
+        Connection connection = connector.getConnection();
+
+        try {
+            PreparedStatement statement = connection.prepareStatement(GET_RECIPE_BY_ID);
+            statement.setInt(1, id);
+            ResultSet resultSet = statement.executeQuery();
+
+            if (resultSet.next()) {
+
+                Recipe recipe;
+                recipe = new Recipe(
+                        resultSet.getString("name"),
+                        resultSet.getString("description"),
+                        resultSet.getString("executable_path"),
+                        resultSet.getInt("engine_id"),
+                        resultSet.getString("args")
+                );
+
+                recipe.id = resultSet.getInt("id");
+                recipe.exists = true;
+
+                // connection.close();
+
+                return recipe;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+
+    public static Recipe getRecipeByName(String slug, String name) {
+        PostgresqlConnector connector = (PostgresqlConnector ) 
+            SystemConnector.getInstance().getDTconnector(slug);
+
+        Connection connection = connector.getConnection();
+
+        try {
+            PreparedStatement statement = connection.prepareStatement(GET_RECIPE_BY_NAME);
+            statement.setString(1, name);
+            ResultSet resultSet = statement.executeQuery();
+
+            if (resultSet.next()) {
+
+                Recipe recipe;
+                recipe = new Recipe(
+                    resultSet.getString("name"),
+                    resultSet.getString("description"),
+                    resultSet.getString("executable_path"),
+                    resultSet.getInt("engine_id"),
+                    resultSet.getString("args")
+                );
+
+                recipe.id = resultSet.getInt("id");
+                recipe.exists = true;
+
+                connection.close();
+                return recipe;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    public void save(String slug) throws SQLException, UnsupportedOperationException {
+        if (!this.exists) {
+            // The object does not exist, it should be inserted.
+            PostgresqlConnector connector = (PostgresqlConnector ) 
+                SystemConnector.getInstance().getDTconnector(slug);
+
+            Connection connection = connector.getConnection();
+
+            PreparedStatement statement = connection.prepareStatement(INSERT_RECIPE_QUERY);
+
+            statement.setString(1, this.name);
+            statement.setString(2, this.description);
+            statement.setString(3, this.executablePath);
+            statement.setInt(4, Integer.valueOf(this.engineId));
+            statement.setString(5, this.args.toString());
+
+            try {
+                ResultSet resultSet = statement.executeQuery();
+
+                if (resultSet.next()) {
+                    this.id = resultSet.getInt("id");
+                }
+
+                connection.commit();
+            } catch (SQLException e) {
+                connection.rollback();
+                throw e;
+            } finally {
+                connection.close();
+            }
+        } else {
+            // The object exists, it should be updated.
+            PostgresqlConnector connector = (PostgresqlConnector ) 
+                SystemConnector.getInstance().getDTconnector(slug);
+
+            Connection connection = connector.getConnection();
+
+            PreparedStatement statement = connection.prepareStatement(UPDATE_RECIPE_QUERY);
+
+            statement.setString(1, this.name);
+            statement.setString(2, this.description);
+            statement.setString(3, this.executablePath);
+            statement.setInt(4, Integer.valueOf(this.engineId));
+            statement.setString(5, this.args.toString());
+            statement.setInt(6, Integer.valueOf(this.id));
+
+            try {
+                statement.executeUpdate();
+
+                connection.commit();
+            } catch (SQLException e) {
+                connection.rollback();
+                throw e;
+            } finally {
+                connection.close();
+            }
+        }
+     }
+}
