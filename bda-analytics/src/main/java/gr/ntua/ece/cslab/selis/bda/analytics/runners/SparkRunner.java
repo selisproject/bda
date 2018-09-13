252f2450cdf2ceb403ac7a9d1f158232edab9a80
Tasos Bakogiannis <abk@cslab.ece.ntua.gr>
2018-09-13 18:25:39 +0300

    limit yarn cache size, very initial on a recipe library.

--- bda-analytics/src/main/java/gr/ntua/ece/cslab/selis/bda/analytics/runners/SparkRunner.java
+++ bda-analytics/src/main/java/gr/ntua/ece/cslab/selis/bda/analytics/runners/SparkRunner.java
@@ -5,8 +5,10 @@ import org.apache.spark.launcher.SparkAppHandle;
 import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.sql.SQLException;
 
 import gr.ntua.ece.cslab.selis.bda.common.Configuration;
+import gr.ntua.ece.cslab.selis.bda.common.storage.beans.ScnDbInfo;
 import gr.ntua.ece.cslab.selis.bda.analytics.basicObjects.ExecutEngineDescriptor;
 import gr.ntua.ece.cslab.selis.bda.analytics.basicObjects.KpiDescriptor;
 import gr.ntua.ece.cslab.selis.bda.kpidb.KPIBackend;
@@ -41,25 +43,40 @@ public class SparkRunner extends ArgumentParser implements Runnable {
                 SparkLauncher launcher = new SparkLauncher()
                     .setMaster(configuration.execEngine.getSparkMaster())
                     .setDeployMode(configuration.execEngine.getSparkDeployMode())
-                    .setConf(SparkLauncher.DRIVER_MEMORY, 
+                    .setConf(SparkLauncher.DRIVER_MEMORY,
                              configuration.execEngine.getSparkConfDriverMemory())
-                    .setConf(SparkLauncher.EXECUTOR_MEMORY, 
+                    .setConf(SparkLauncher.EXECUTOR_MEMORY,
                              configuration.execEngine.getSparkConfExecutorMemory())
-                    .setConf(SparkLauncher.EXECUTOR_CORES, 
+                    .setConf(SparkLauncher.EXECUTOR_CORES,
                              configuration.execEngine.getSparkConfExecutorCores())
                     .setAppResource(this.recipeResource);
 
                 if (configuration.execEngine.getSparkConfJars() != null) {
-                    launcher.addSparkArg("--jars", configuration.execEngine.getSparkConfJars());
+                    launcher.addSparkArg("--jars",
+                                         configuration.execEngine.getSparkConfJars());
+                    launcher.addSparkArg("--driver-class-path",
+                                         configuration.execEngine.getSparkConfJars());
                 }
                 if (message != "") {
                     launcher.addAppArgs(message);
                 }
 
+                ScnDbInfo scn =  ScnDbInfo.getScnDbInfoBySlug(scnSlug);
+
+                launcher.addAppArgs(
+                    "--selis-scn-db", scn.getDtDbname(),
+                    "--selis-dt-url", configuration.storageBackend.getDimensionTablesURL(),
+                    "--selis-dt-user", configuration.storageBackend.getDbUsername(),
+                    "--selis-dt-pass", configuration.storageBackend.getDbPassword()
+                );
+
                 handle = launcher.startApplication();
             } catch (IOException e) {
                 e.printStackTrace();
                 LOGGER.log(Level.WARNING,"Spark job failed to start!");
+            } catch (SQLException e) {
+                e.printStackTrace();
+                LOGGER.log(Level.WARNING,"Spark job failed. Unknown SCN.");
             }
 
             try {
