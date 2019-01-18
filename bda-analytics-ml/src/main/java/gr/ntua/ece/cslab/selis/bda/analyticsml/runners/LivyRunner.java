a0244df5206bad3533a80f937e5dae1bb22afb2c
eviekassela <eviekassel@gmail.com>
2019-01-18 15:49:15 +0200

    final working code for livy

--- bda-analytics-ml/src/main/java/gr/ntua/ece/cslab/selis/bda/analyticsml/runners/LivyRunner.java
+++ bda-analytics-ml/src/main/java/gr/ntua/ece/cslab/selis/bda/analyticsml/runners/LivyRunner.java
@@ -1,7 +1,6 @@
 package gr.ntua.ece.cslab.selis.bda.analyticsml.runners;
 
 import gr.ntua.ece.cslab.selis.bda.common.Configuration;
-import gr.ntua.ece.cslab.selis.bda.common.storage.SystemConnectorException;
 import gr.ntua.ece.cslab.selis.bda.common.storage.beans.ExecutionEngine;
 import gr.ntua.ece.cslab.selis.bda.common.storage.beans.ScnDbInfo;
 import gr.ntua.ece.cslab.selis.bda.datastore.beans.Recipe;
@@ -9,19 +8,22 @@ import gr.ntua.ece.cslab.selis.bda.datastore.beans.Recipe;
 import org.json.JSONObject;
 import javax.ws.rs.client.*;
 import javax.ws.rs.core.Response;
-import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class LivyRunner extends ArgumentParser implements Runnable {
     private final static Logger LOGGER = Logger.getLogger(LivyRunner.class.getCanonicalName());
 
-    String messageId;
-    String scnSlug;
-    Recipe recipe;
-    ExecutionEngine engine;
+    private String messageId;
+    private String scnSlug;
+    private Recipe recipe;
+    private ExecutionEngine engine;
+    private WebTarget resource;
+    private Configuration configuration;
 
     public LivyRunner(Recipe recipe, ExecutionEngine engine,
                        String messageId, String scnSlug) {
@@ -30,38 +32,55 @@ public class LivyRunner extends ArgumentParser implements Runnable {
         this.scnSlug = scnSlug;
         this.recipe = recipe;
         this.engine = engine;
+
+        this.configuration = Configuration.getInstance();
+
+        Client client = ClientBuilder.newClient();
+        this.resource = client.target(configuration.execEngine.getLivyURL());
+
+    }
+
+    private void deleteSession(String sessionId){
+        Invocation.Builder request = resource.path("/sessions/"+sessionId).request();
+        Response response = request.delete();
+        if (response.getStatusInfo().getFamily() == Response.Status.Family.SUCCESSFUL) {
+            LOGGER.log(Level.INFO,
+                    "SUCCESS: Session has been deleted.");
+        } else {
+            LOGGER.log(Level.SEVERE,
+                    "Request to delete session has failed, got error: {0}",
+                    response.getStatusInfo().getReasonPhrase());
+        }
     }
 
     @Override
     public void run() {
-        ScnDbInfo scn = null;
+        ScnDbInfo scn;
         try {
             scn = ScnDbInfo.getScnDbInfoBySlug(scnSlug);
-        } catch (SQLException e) {
+        } catch (Exception e) {
             e.printStackTrace();
-        } catch (SystemConnectorException e) {
-            e.printStackTrace();
+            return;
         }
-        Configuration configuration = Configuration.getInstance();
 
-        Client client = ClientBuilder.newClient();
-        WebTarget resource = client.target(configuration.execEngine.getLivyURL());
         String sessionId, statementId;
 
-        Invocation.Builder request = resource.path("/sessions").request();
-
         // Create session and upload binaries
+        Invocation.Builder request = resource.path("/sessions").request();
         JSONObject data = new JSONObject();
         data.put("kind","pyspark");
         List<String> files = new ArrayList<>();
         files.add(recipe.getExecutablePath());
-        //files.add("/RecipeDataLoader.py");
+        //files.add("file:///resources/RecipeDataLoader.py");
         data.put("files", files);
         if (configuration.execEngine.getSparkConfJars() != null) {
             List<String> jars = new ArrayList<>();
-            jars.add("local:/"+configuration.execEngine.getSparkConfJars());
+            jars.add("file://"+configuration.execEngine.getSparkConfJars());
             data.put("jars", jars);
-            //data.put("conf", "{\"--driver-class-path\",\"" + configuration.execEngine.getSparkConfJars()+ "\"}");
+
+            Map<String, String> classpath = new HashMap<>();
+            classpath.put("spark.driver.extraClassPath","file://"+configuration.execEngine.getSparkConfJars());
+            data.put("conf", classpath);
         }
         data.put("driverMemory", configuration.execEngine.getSparkConfDriverMemory());
         data.put("executorMemory", configuration.execEngine.getSparkConfExecutorMemory());
@@ -70,9 +89,8 @@ public class LivyRunner extends ArgumentParser implements Runnable {
         Response response = request.post(Entity.json(data.toString()));
         if (response.getStatusInfo().getFamily() == Response.Status.Family.SUCCESSFUL) {
             LOGGER.log(Level.INFO,
-                    "SUCCESS: Request to create session for executable has been sent.");
+                    "SUCCESS: Request to create session has been sent. Waiting for session to start..");
             sessionId = new JSONObject(response.readEntity(String.class)).get("id").toString();
-            LOGGER.log(Level.INFO, sessionId);
         } else {
             LOGGER.log(Level.SEVERE,
                     "Request to create session has failed, got error: {0}",
@@ -101,6 +119,7 @@ public class LivyRunner extends ArgumentParser implements Runnable {
                 LOGGER.log(Level.SEVERE,
                         "Request to get session status has failed, got error: {0}",
                         response.getStatusInfo().getReasonPhrase());
+                deleteSession(sessionId);
                 return;
             }
         }
@@ -116,13 +135,13 @@ public class LivyRunner extends ArgumentParser implements Runnable {
         response = request.post(Entity.json(data.toString()));
         if (response.getStatusInfo().getFamily() == Response.Status.Family.SUCCESSFUL) {
             LOGGER.log(Level.INFO,
-                    "SUCCESS: Request to create session for executable has been sent.");
+                    "SUCCESS: Job has been submitted.");
             statementId = new JSONObject(response.readEntity(String.class)).get("id").toString();
-            LOGGER.log(Level.INFO, statementId);
         } else {
             LOGGER.log(Level.SEVERE,
                     "Request to launch job has failed, got error: {0}",
                     response.getStatusInfo().getReasonPhrase());
+            deleteSession(sessionId);
             return;
         }
 
@@ -137,7 +156,8 @@ public class LivyRunner extends ArgumentParser implements Runnable {
                 result = new JSONObject(response.readEntity(String.class));
                 state = result.get("state").toString();
                 if (state.matches("error") || state.contains("cancel")){
-                    LOGGER.log(Level.SEVERE, "Job has failed, status: {0}", state);
+                    LOGGER.log(Level.SEVERE, "Job failed, status: {0}", result.get("output"));
+                    deleteSession(sessionId);
                     return;
                 }
                 try {
@@ -149,10 +169,14 @@ public class LivyRunner extends ArgumentParser implements Runnable {
                 LOGGER.log(Level.SEVERE,
                         "Request to get job status has failed, got error: {0}",
                         response.getStatusInfo().getReasonPhrase());
+                deleteSession(sessionId);
                 return;
             }
         }
         String output = new JSONObject(result.get("output").toString()).get("data").toString();
-        LOGGER.log(Level.INFO, output);
+        LOGGER.log(Level.INFO, "Job result: " +output);
+
+        // Delete session
+        deleteSession(sessionId);
     }
 }
