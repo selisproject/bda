0c6e87590fb016c1993425fd566fd0d38573ef91
eviekassela <eviekassel@gmail.com>
2019-03-26 13:43:53 +0200

    work with public and secure pubsub server

--- bda-controller/src/main/java/gr/ntua/ece/cslab/selis/bda/controller/connectors/PubSubSubscriber.java
+++ bda-controller/src/main/java/gr/ntua/ece/cslab/selis/bda/controller/connectors/PubSubSubscriber.java
@@ -6,6 +6,20 @@ import gr.ntua.ece.cslab.selis.bda.controller.beans.PubSubSubscription;
 import gr.ntua.ece.cslab.selis.bda.datastore.beans.Tuple;
 import gr.ntua.ece.cslab.selis.bda.datastore.beans.KeyValue;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -38,6 +52,51 @@ public class PubSubSubscriber implements Runnable {
         this.reloadSubscriptionsFlag = true;
     }
 
+    public String getAccessToken(String username, String password) {
+        String POST_URL = "https://selis-gw.cslab.ece.ntua.gr:8443/auth/realms/selisrealm/protocol/openid-connect/token";
+
+        HttpClient httpclient = HttpClients.createDefault();
+        HttpPost httppost = new HttpPost(POST_URL);
+
+        // Request parameters and other properties.
+        List<NameValuePair> params = new ArrayList<>();
+        params.add(new BasicNameValuePair("client_id", "pubsub_client"));
+        params.add(new BasicNameValuePair("username", username));
+        params.add(new BasicNameValuePair("password", password));
+        params.add(new BasicNameValuePair("grant_type", "password"));
+        params.add(new BasicNameValuePair("client_secret", "pubsub_secret"));
+
+        try {
+            httppost.setEntity(new UrlEncodedFormEntity(params, "UTF-8"));
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+            return "";
+        }
+
+        //Execute and get the response.
+        HttpResponse response = null;
+        try {
+            response = httpclient.execute(httppost);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return "";
+        }
+
+        String results = null;
+        try {
+            results = EntityUtils.toString(response.getEntity());
+        } catch (IOException e) {
+            e.printStackTrace();
+            return "";
+        }
+
+        JSONObject responseObject = new JSONObject(results);
+
+        //System.out.println(responseObject);
+
+        return (String) responseObject.get("access_token");
+    }
+
     @Override
     public void run() {
         PubSub pubsub = null;
@@ -55,7 +114,8 @@ public class PubSubSubscriber implements Runnable {
                 if (!(subscriptions.getSubscriptions().isEmpty())) {
 
                     for (Tuple messageTypeName : subscriptions.getSubscriptions()) {
-                        Subscription subscription = new Subscription(this.authHash);
+                        String authHash = getAccessToken("selis_bda", "bda_secret");
+                        Subscription subscription = new Subscription(authHash);
 
                         for (KeyValue rule: messageTypeName.getTuple())
                             subscription.add(new Rule(rule.getKey(), rule.getValue(), RuleType.EQ));
